cee3aee6116ff0f4619f37b4723f2a4bd02f837e
bnnebedum <148578923+bnnebedum@users.noreply.github.com>
2023-10-20 13:50:36 -0400

    Add files via upload

---
+++ Project 3/DecimalToBinary.h
@@ -0,0 +1,56 @@
+// Bede Nnebedum
+// CSDP 250 - Fall 2023
+// Project 3
+
+#ifndef DecimalToBinary_h
+#define DecimalToBinary_h
+
+using namespace std;
+
+class Stack
+{
+private:
+    struct StackNode
+    {
+        int value;
+        StackNode *next;
+    };
+    StackNode *top;
+    void push(int);
+    void pop();
+
+public:
+    Stack()
+    {
+        top = nullptr;
+    }
+    ~Stack() {}
+    void createStack(int);
+    void displayStack();
+};
+
+class Queue
+{
+private:
+    struct QueueNode
+    {
+        int value;
+        QueueNode *next;
+    };
+    QueueNode *front;
+    QueueNode *rear;
+    void enqueue(int);
+    void dequeue();
+
+public:
+    Queue()
+    {
+        front = nullptr;
+        rear = nullptr;
+    }
+    ~Queue() {}
+    void createQueue(double);
+    void displayQueue();
+};
+
+#endif
