79ac154242ae9db110043562a4f70077ea004741
bnnebedum <148578923+bnnebedum@users.noreply.github.com>
2023-10-20 17:25:55 -0400

    Add files via upload

---
+++ Project 3/main.cpp
@@ -0,0 +1,46 @@
+// Bede Nnebedum
+// CSDP 250 - Fall 2023
+// Project 3
+
+#include <iostream>
+#include "DecimalToBinary.h"
+#include "DecimalToBinary.cpp"
+
+using namespace std;
+
+int main()
+{
+    Stack stack;
+    Queue queue;
+    double num;
+    int places;
+
+    cout << endl << "Enter decimal: ";
+    cin >> num;
+
+    int intpart = num;
+    double decpart = num - intpart;
+
+    stack.createStack(intpart);
+    if (decpart != 0)
+    {
+        do
+        {
+            cout << "Number of decimal places in final answer: ";
+            cin >> places;
+        } while (places < 1);
+        queue.createQueue(decpart, places);
+    }
+
+    cout << endl
+         << "Binary conversion: ";
+    stack.displayStack();
+
+    if (decpart != 0)
+    {
+        cout << ".";
+        queue.displayQueue();
+    }
+
+    cout << endl;
+}
